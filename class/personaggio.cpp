8edf1664edb4f79e074bf7c6f2e90f8b725f8aa1
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-11-17 15:42:05 +0100

    ho implementato il salto in modo che salti in maniera più fluida anche senza input

--- class/personaggio.cpp
+++ class/personaggio.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <ctime>
 #include "personaggio.hpp"
-Player::Player(int startX, int startY, int jump_m, Map *m) : x(startX), jump_max(jump_m), y(startY), map(m), jumpHeight(7), isAlive(true), isJumping(false), gravity(1), bodyHeight(1) {}
+Player::Player(int startX, int startY,int jump_m, Map *m) : x(startX),jump_max(jump_m), y(startY), map(m), jumpHeight(7), isAlive(true), isJumping(false), gravity(1), bodyHeight(1){}
 void Player::draw(WINDOW *game)
 {
     mvwprintw(game, y, x, "@");
@@ -23,60 +23,59 @@ void Player::init()
         }
     }
     x = 1;
-    y = MAX_Y - 2; //- bodyHeight; // Posiziona il baricentro dello stickman sotto le gambe, sopra la piattaforma più bassa trovata
+    y = MAX_Y-2; //- bodyHeight; // Posiziona il baricentro dello stickman sotto le gambe, sopra la piattaforma più bassa trovata
 }
 void Player::p_move(WINDOW *game, char m = 'r')
 {
     int oldx = x;
     int oldy = y;
 
-    // Salva la posizione corrente del cursore
+        // Salva la posizione corrente del cursore
 
-    // Itera attraverso le posizioni intermedie
-    /*for (int i = 0; i < 5; i++) {
-         // Sposta il cursore alla posizione desiderata
-        y=y-1;
-        move(x,y);
-
-
-         // Stampa il carattere
-        //draw(win);
-
-        // Attendi un breve intervallo di tempo
-        napms(100);
-    }
-    for (int i = 0; i < 5; i++) {
-    // Sposta il cursore alla posizione desiderata
-        y=y+1;
-        move(x,y);
+        // Itera attraverso le posizioni intermedie
+        /*for (int i = 0; i < 5; i++) {
+             // Sposta il cursore alla posizione desiderata
+            y=y-1;
+            move(x,y);
 
 
-         // Stampa il carattere
-        //draw(win);
+             // Stampa il carattere
+            //draw(win);
 
-        // Attendi un breve intervallo di tempo
-        napms(100);
-    }
+            // Attendi un breve intervallo di tempo
+            napms(100);
+        }
+        for (int i = 0; i < 5; i++) {
+        // Sposta il cursore alla posizione desiderata
+            y=y+1;
+            move(x,y);
+
+
+             // Stampa il carattere
+            //draw(win);
+
+            // Attendi un breve intervallo di tempo
+            napms(100);
+        }
 
 // Ripristina la posizione del cursore
-     move(oldx, oldy);*/
-    //   isJumping = true;
-    if (m == 'l' && x > 1)
-    {
+         move(oldx, oldy);*/
+     //   isJumping = true;
+   if (m == 'l' && x > 1){
         mvwaddch(game, y, x, ' '); // Cancellazione personaggio corrente
         x -= 1;
         draw(game); // Disegno del personaggio nella nuova posizione
-    }
-    // Controllo se il giocatore è caduto dalla mappa
-    if (x < 0)
-    {
-        isAlive = false;
-    }
-    if (m == 'r' && x < MAX_X - 1)
-    {
+   }
+        // Controllo se il giocatore è caduto dalla mappa
+        if (x < 0)
+        {
+            isAlive = false;
+        }
+    if (m == 'r' && x<MAX_X-1){
         mvwaddch(game, y, x, ' '); // Cancellazione personaggio corrente
         x += 1;
         draw(game); // Disegno del personaggio nella nuova posizione
+     
     }
     while (!map->platformUnder(x, y) && !this->isJumping){
       mvwaddch(game, y, x, ' '); // Cancellazione personaggio corrente
@@ -88,66 +87,67 @@ void Player::p_move(WINDOW *game, char m = 'r')
 void Player::jump(WINDOW *game) {
     this->isJumping = true;
     int i = 1;
-    bool a = true;
-      cbreak();
-             nodelay(game, true);
-    noecho();
-    while ((i <= this->jump_max) && a) {
-        int ch = getch();
-                if (map->platformAbove(x, y)) {
-                a = false;
-            } else {
-                y = y - 1;
-                i++;
-            }
-        mvwaddch(game, y+1, x, ' ');
-        wrefresh(game);
-        if (ch == 'a' || ch == 'A' || ch == KEY_LEFT) {
-            if (!map->isPlatform(x-1, y))
-            this->p_move(game, 'l');
-        } else
-        if (ch == 'd' || ch == 'D' || ch == KEY_RIGHT) {
-            if (!map->isPlatform(x-1, y))
-            this->p_move(game, 'r');
-        } else {
-        napms(100);
-        mvwaddch(game, y, x, '@');
-        wrefresh(game);
-        }
-    }
-a = true;
-while (i != 1 && a){
- int ch = getch();
-                if (map->platformUnder(x, y)) {
-                a = false;
-            } else {
-                y = y + 1;
-                i--;
-            }
-        mvwaddch(game, y-1, x, ' ');
-        wrefresh(game);
-        if (ch == 'a' || ch == 'A' || ch == KEY_LEFT) {
-            if (!map->isPlatform(x+1, y))
-            this->p_move(game, 'l');
-        } else
-        if (ch == 'd' || ch == 'D' || ch == KEY_RIGHT) {
-            if (!map->isPlatform(x+1, y))
-            this->p_move(game, 'r');
-        } else {
-        napms(100);
-        mvwaddch(game, y, x, '@');
-        wrefresh(game);
-        }
-        }
-    this->isJumping = false;
-        // Disegna il personaggio nella nuova posizione
 
-
-        // Attendi brevemente
+    while (i <= this->jump_max) {
         
+        if (map->platformAbove(x, y)) {
+            break;  // Interrompi il salto se c'è una piattaforma sopra.
+        } else {
+            y = y - 1;
+            i++;
+        }
 
-        // Aggiorna il contatore i in base alla direzione del salto
+        mvwaddch(game, y + 1, x, ' ');
+        wrefresh(game);
+        napms(50);
+        mvwaddch(game, y, x, '@');
+        wrefresh(game);
+
+        // Effettua un passo laterale durante il salto se viene premuto un tasto.
+        
+        int ch = getch();
+        if (ch == 'a' || ch == 'A' || ch == KEY_LEFT) {
+            if (!map->isPlatform(x - 1, y))
+                this->p_move(game, 'l');
+        } else if (ch == 'd' || ch == 'D' || ch == KEY_RIGHT) {
+            if (!map->isPlatform(x + 1, y))
+                this->p_move(game, 'r');
+        }
     }
+
+    // Ripristina la posizione iniziale del personaggio dopo il salto.
+    while (i != 1) {
+        if (map->platformUnder(x, y)) {
+            break;  // Interrompi la discesa se c'è una piattaforma sotto.
+        } else {
+            y = y + 1;
+            i--;
+        }
+
+        mvwaddch(game, y - 1, x, ' ');
+        wrefresh(game);
+        napms(50);
+        mvwaddch(game, y, x, '@');
+        wrefresh(game);
+
+        int ch = getch();
+        if (ch == 'a' || ch == 'A' || ch == KEY_LEFT) {
+            if (!map->isPlatform(x - 1, y))
+                this->p_move(game, 'l');
+        } else if (ch == 'd' || ch == 'D' || ch == KEY_RIGHT) {
+            if (!map->isPlatform(x + 1, y))
+                this->p_move(game, 'r');
+        }
+    }
+
+    // Fine del salto, reimposta 'isJumping' a false.
+    this->isJumping = false;
+}
+
+
+
+        
+       
 
 void Player::updateJump()
 {
@@ -169,7 +169,7 @@ void Player::updateJump()
             }
         }
         napms(500);
-        while (i >= 1)
+        while (i >=1)
         {
             y++;
 
@@ -202,4 +202,5 @@ void Player::checkCollision()
         isJumping = false;
         jumpHeight = 0;
     }
+}
 }
