45dddeb1b4326cb33080b60eafbb50bec7470434
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-11-18 16:58:03 +0100

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -1,15 +1,19 @@
 #include "class/layout.hpp"
+#include "class/enemy.hpp"
 #include "class/personaggio.hpp"
 #include "class/map.hpp"
+
+
 class Game
 {
 private:
   Layout *layout;
   Player *player;
   Map *map;
+  Nemico *enemy;
   bool quit = false;
 
 public:
-  Game(Layout *l, Map *m, Player *p);
+  Game(Layout *l, Map *m, Player *p, Nemico *en);
   void run();
 };
