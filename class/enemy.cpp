6bef075e19418b370b2326550059cca9da2774da
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-11-18 16:54:10 +0100

    Update enemy.cpp

--- class/enemy.cpp
+++ class/enemy.cpp
@@ -1,3 +1,42 @@
 #include <ncurses.h>
 #include <iostream>
 #include <ctime>
+#include "enemy.hpp"
+
+    Nemico::Nemico(int x, int y) : x_(x), y_(y) {}
+
+    void Nemico::draw(WINDOW *win)
+    {
+        mvwprintw(win, y_, x_, "X");
+    }
+    void Nemico::cancella(WINDOW *win)
+    {
+        mvwprintw(win, y_, x_, " ");
+    }
+    void Nemico::move(int playerX, int playerY)
+    {
+        // Calcola la direzione verso il giocatore
+        int deltaX = playerX - x_;
+        int deltaY = playerY - y_;
+
+        // Sposta il nemico verso il giocatore
+        if (deltaX > 0)
+        {
+            x_++;
+        }
+        else if (deltaX < 0)
+        {
+            x_--;
+        }
+
+        if (deltaY > 0)
+        {
+            y_++;
+        }
+        else if (deltaY < 0)
+        {
+            y_--;
+        }
+    }
+
+
