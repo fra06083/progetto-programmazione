c257a0a2f7795727ce4382ae8887530112ad9e8d
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-11-18 16:59:36 +0100

    aggiornato il comando di compilazione ed i parametri legati a game

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-//comando compilazione: g++ main.cpp class/layout.cpp class/map.cpp class/personaggio.cpp game.cpp -lncurses -o test
+//comando compilazione: g++ main.cpp class/layout.cpp class/map.cpp class/personaggio.cpp game.cpp class/enemy.cpp -lncurses -o test
 // #include "class/personaggio.hpp"
 // #include "class/map.hpp"
 #include "game.hpp"
@@ -12,8 +12,9 @@ int main(int argc, char **argv)
     // qui faremo la dichiarazione dei puntatori con la macroclasse che può gestirli
     Layout *layout = new Layout();
     Map *map = new Map();
-    Player *player = new Player(MAX_X-START_X+1, MAX_Y-2, 8, map); // refers to layout
-    Game *game = new Game(layout, map, player);
+    Player *player = new Player(MAX_X-START_X+1, MAX_Y-2,8, map); // refers to layout
+    Nemico *enemy = new Nemico(50,12);
+    Game *game = new Game(layout, map, player, enemy);
     game->run(); // LOOP per non chiudere il gioco
 
     return 0;
