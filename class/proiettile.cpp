c0ef5b3f07a0d67c6ffc4271b31c48fadaa98e3c
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-12-01 16:08:14 +0100

    aggiornate move e tail_delete

--- class/proiettile.cpp
+++ class/proiettile.cpp
@@ -1,89 +1,83 @@
-#include "proiettile.hpp"
+   #include "proiettile.hpp"
+   
+    Proiettile::Proiettile(int startX, int startY) : x(startX), y(startY) {}
 
-Proiettile::Proiettile(int startX, int startY) : x(startX), y(startY) {}
-
-void Proiettile::spara()
-{
-    sparato = true;
-}
-bool Proiettile::isAttivo()
-{
-    if (this == nullptr)
-    {
+    void Proiettile::spara() {
+        sparato = true;
+    }
+    bool Proiettile::isAttivo(){
+        if (this == nullptr) {
         // Il puntatore this non è valido
         return false;
     }
-    return sparato && x < 80; // Aggiungi screenWidth come membro della classe
+    return sparato && x < 80;  // Aggiungi screenWidth come membro della classe
 }
 
-void Proiettile::move(WINDOW *game, lista_proiettili *proiettile)
-{
-    if (proiettile != NULL)
-    {
-        mvwaddch(game, y, x, ' ');
-        wrefresh(game);
-        // napms(50);
-        x = x + 1;
-        mvwaddch(game, y, x, '*');
-        wrefresh(game);
+    void Proiettile::move(WINDOW *game, lista_proiettili *proiettile, char dir) {
+            if(proiettile!=NULL){
+            mvwaddch(game, y, x, ' ');
+            wrefresh(game);
+           // napms(50);
+            if(dir=='r'){
+                x=x+1;
+            }
+            if(dir=='l'){
+                x=x-1;
+            }
+            mvwaddch(game, y, x, '*');
+            wrefresh(game);
+            
+            mvwaddch(game, y, x, ' ');
+            wrefresh(game);
+           // napms(50);
+            if(dir=='r'){
+                x=x+1;
+            }
+            if(dir=='l'){
+                x=x-1;
+            }
+            mvwaddch(game, y, x, '*');
+            wrefresh(game);}
 
-        mvwaddch(game, y, x, ' ');
-        wrefresh(game);
-        // napms(50);
-        x = x + 1;
-        mvwaddch(game, y, x, '*');
-        wrefresh(game);
     }
-}
 
-void Proiettile::draw(WINDOW *game, int playerX, int playerY)
-{
 
-    x = playerX + 1;
-    y = playerY;
-    mvwaddch(game, y, x, '*');
-    wrefresh(game);
-}
-p_pro head_insert(p_pro &list, Proiettile *pr)
-{
-    p_pro tmp = new lista_proiettili;
-    tmp->pro = pr;
-    tmp->next = list;
-    list = tmp;
+    void Proiettile::draw(WINDOW *game, int playerX, int playerY) {
 
+            x=playerX+1;
+            y=playerY;
+            mvwaddch(game,y,x, '*');
+            wrefresh(game);
+    
+    }
+p_pro head_insert(p_pro& list, Proiettile* pr){
+        p_pro tmp=new lista_proiettili;
+        tmp->pro= pr;
+        tmp->next=list;
+        list=tmp;
+    
     return list;
 }
 
-p_pro tail_delete(p_pro list)
-{
+p_pro tail_delete(p_pro list, Map *map) {
     p_pro prevNode = NULL;
     p_pro currentNode = list;
 
-    while (currentNode != NULL)
-    {
-        if (currentNode->pro->x >= 80)
-        {
+    while (currentNode != NULL) {
+        if (currentNode->pro->x >= 80 || map->isPlatform(currentNode->pro->x+1,currentNode->pro->y)) {
             p_pro nextNode = currentNode->next;
             delete currentNode->pro;
             delete currentNode;
 
-            if (prevNode != NULL)
-            {
+            if (prevNode != NULL) {
                 prevNode->next = nextNode;
-            }
-            else
-            {
+            } else {
                 list = nextNode;
             }
 
             currentNode = nextNode;
-        }
-        else
-        {
+        } else {
             prevNode = currentNode;
             currentNode = currentNode->next;
         }
     }
-
-    return list;
-}
