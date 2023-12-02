53d589df95de0651408c8e6b0945f8a394addce2
EmiGiac333 <131464246+EmiGiac333@users.noreply.github.com>
2023-12-02 12:04:29 +0100

    creata lista nemici

--- game.hpp
+++ game.hpp
@@ -4,6 +4,8 @@
 #include "class/personaggio.hpp"
 #include "class/map.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
@@ -13,17 +15,17 @@ private:
   Layout *layout;
   Player *player;
   Map *map;
-  Nemico *enemy;
+  p_en enemy;
   p_pro proiettile;
   bool quit = false;
   int counter=0;
+
 public:
-  Game(Layout *l, Map *m, Player *p, Nemico *en, p_pro pr);
+  Game(Layout *l, Map *m, Player *p, p_en en, p_pro pr);
   int updateJump(WINDOW* game, Player *p, Map *map, bool isJump, int i);
   void run();
 
 };
-
 
 
 
