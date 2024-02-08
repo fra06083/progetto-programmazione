692be48090c1208515f0c682024e787da5bc38b1
francesco biglieri <130977365+fra06083@users.noreply.github.com>
2024-02-08 14:59:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 
 //comando compilazione: g++ main.cpp savingSystem/SaveLoad.cpp class/layout.cpp class/objects.cpp class/map/room.cpp class/map/map.cpp class/map/platform.cpp class/personaggio.cpp  class/enemy.cpp class/proiettile.cpp game.cpp -lncurses -o test
-// #include "class/personaggio.hpp"
-// #include "class/map.hpp"
 #include "game.hpp"
 #include <iostream>
 #include <ctime>
@@ -9,16 +7,16 @@
 
 int main(int argc, char **argv)
 {
-    // qui faremo la dichiarazione dei puntatori con la macroclasse che può gestirli
+    // dichiarazioni puntatori che verranno passati alla claasse GAME
     Layout *layout = new Layout();
     Map *map = new Map();
-    Player *player = new Player(MAX_X-START_X+1, MAX_Y-2, 5, map, 40); // il valore 40 è la vita, la prenderà dal salvataggio.
+    Player *player = new Player(MAX_X-START_X+1, MAX_Y-2, 5, map, 40); // Impostazione iniziale del personaggio.
     p_base_en base_enemy = NULL;
    
     p_pro proiettile=NULL;
     Game *game = new Game(layout, map, player, base_enemy, proiettile);
 
-    game->run(); // LOOP per non chiudere il gioco
+    game->run(); // LOOP per non far chiudere il gioco
 
     return 0;
 }
